92ec8b29131f6b3f4dd792c2e4b8a80fe4f4dc8f
Margot De maulmont <mde-maul@c1r6p12.hive.fi>
2022-05-26 15:01:58 +0300

    get_frequency function

--- main3.c
+++ main3.c
@@ -1,13 +1,76 @@
 #include <stdio.h>
 #include "SDL2/SDL.h"
+#include "libft/libft.h"
 #define PI2 6.28318530718
 
-int main(void) {
+int notes_match(char *note1, char *note2, int char_nb)
+{
+	if (ft_strncmp(note1, note2, char_nb) == 0)
+		return (1);
+	return (0);
+}
 
-	float freq = 440; // a
-	//freq = 261.6256; // c
-	float freq2 = 261.6256; // c
-	float freq3 = 880; // c
+double	get_note(char *note)
+{
+	double frequency = 0.0;
+
+	if (notes_match("c", note, 1))
+		frequency = 16.35160;
+	if (notes_match("c#", note, 2))
+		frequency = 17.32391;
+	if (notes_match("d", note, 1))
+		frequency = 18.35405;
+	if (notes_match("d#", note, 2))
+		frequency = 19.44544;
+	if (notes_match("e", note, 1))
+		frequency = 20.60172;
+	if (notes_match("f", note, 1))
+		frequency = 21.82676;
+	if (notes_match("f#", note, 2))
+		frequency = 23.12465;
+	if (notes_match("g", note, 1))
+		frequency = 24.49971;
+	if (notes_match("g#", note, 2))
+		frequency = 25.95654;
+	if (notes_match("a", note, 1))
+		frequency = 27.50000;
+	if (notes_match("a#", note, 2))
+		frequency = 29.13524;
+	if (notes_match("b", note, 1))
+		frequency = 30.86771;
+	return (frequency);
+}
+
+float get_frequency(char *note)
+{
+	float frequency;
+	int octave;
+	size_t len;
+
+	len = ft_strlen(note);
+	if (ft_strchr(note, '#'))
+	{
+		if (len == 2)
+			octave = 4;
+		else
+			octave = ft_atoi(&note[2]);
+	}
+	else
+	{
+		if (len == 1)
+			octave = 4;
+		else
+			octave = ft_atoi(&note[1]);
+	}
+	frequency = get_note(note) * pow(2, octave);
+	//printf("octave is %d\n", octave);
+	return (frequency);
+}
+
+int main(void) {
+	float note1 = get_frequency("a4");
+	float note2 = get_frequency("c4");
+	//printf("get frequ b8 %f\n", get_frequency("b8"));
     SDL_Init(SDL_INIT_AUDIO);
 
     // the representation of our audio device in SDL:
@@ -28,7 +91,6 @@ int main(void) {
     // pushing 3 seconds of samples to the audio buffer:
     float x = 0;
     float x2 = 0;
-    float x3 = 0;
     for (int i = 0; i < audio_spec.freq * 2; i++) {
    
 
@@ -36,24 +98,17 @@ int main(void) {
         // note: "5000" here is just gain so that we will hear something
         int16_t sample = sin(x) * 32000;
         int16_t sample2 = sin(x2) * 32000;
-        int16_t sample3 = sin(x3) * 32000;
 
-		x += freq * PI2 / 48000.0;
+		x += note1 * PI2 / 48000.0;
 		if(x >= PI2)
 			x -= PI2;
-		
-		x2 += freq2 * PI2 / 48000.0;
+		x2 += note2 * PI2 / 48000.0;
 		if(x2 >= PI2)
 			x2 -= PI2;
-		x3 += freq3 * PI2 / 48000.0;
-		if(x3 >= PI2)
-			x3 -= PI2;
 
-		(void)sample3;
         const int sample_size = sizeof(int16_t) * 1;
         SDL_QueueAudio(audio_device, &sample, sample_size);
        SDL_QueueAudio(audio_device, &sample2, sample_size);
-       //SDL_QueueAudio(audio_device, &sample3, sample_size);
     }
 
 
