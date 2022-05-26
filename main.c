aff488704448f51cfc08095ffa99e5e7197c2a6f
Roope Vuorenlehto <rvuorenl@c1r3p6.hive.fi>
2022-05-26 21:11:38 +0300

    segfault

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: rvuorenl <rvuorenl@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/25 14:21:09 by rvuorenl          #+#    #+#             */
-/*   Updated: 2022/05/26 16:39:11 by rvuorenl         ###   ########.fr       */
+/*   Updated: 2022/05/26 21:11:15 by rvuorenl         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,13 +15,15 @@
 #include <string.h>	// strncmp
 #include <stdlib.h>	// atoi
 #include <ctype.h>	// isdigit
+#include "./include/minisynth.h"
 
-typedef struct	s_node
-{
-	float 			len;
-	char			*tone;
-	struct	s_node	*next;
-}				t_node;
+// typedef struct	s_node
+// {
+// 	float 			len;
+// 	// char			*tone;
+// 	float			frequency;
+// 	struct	s_node	*next;
+// }				t_node;
 
 // tracks triangle,sine,sine,sine,sine,sine,sine,saw,saw,saw,saw,saw,saw,sine,sine,sine,sine,sine,triangle,triangle,triangle,square,square,square,kick,snare
 
@@ -81,13 +83,118 @@ int	tracks_line(char *str, int *arr)
 // 5 kick
 // 6 snare
 
-// void	read_input(char *str, int *channel, int tracks, t_node *notes)
-void	read_input(char *str, int tempo, t_node *data)
+int notes_match(char *note1, char *note2, int char_nb)
 {
+	if (strncmp(note1, note2, char_nb) == 0)
+		return (1);
+	return (0);
+}
+
+double	get_note(char *note)
+{
+	double frequency = 0.0;
+
+	if (notes_match("c", note, 1))
+		frequency = 16.35160;
+	else if (notes_match("c#", note, 2))
+		frequency = 17.32391;
+	else if (notes_match("d", note, 1))
+		frequency = 18.35405;
+	else if (notes_match("d#", note, 2))
+		frequency = 19.44544;
+	else if (notes_match("e", note, 1))
+		frequency = 20.60172;
+	else if (notes_match("f", note, 1))
+		frequency = 21.82676;
+	else if (notes_match("f#", note, 2))
+		frequency = 23.12465;
+	else if (notes_match("g", note, 1))
+		frequency = 24.49971;
+	else if (notes_match("g#", note, 2))
+		frequency = 25.95654;
+	else if (notes_match("a", note, 1))
+		frequency = 27.50000;
+	else if (notes_match("a#", note, 2))
+		frequency = 29.13524;
+	else if (notes_match("b", note, 1))
+		frequency = 30.86771;
+	return (frequency);
+}
+
+float get_frequency(char *note)
+{
+	float frequency;
+	int octave;
+	size_t len;
+
+	len = strlen(note);
+	if (strchr(note, '#'))
+	{
+		if (len == 2)
+			octave = 4;
+		else
+			octave = atoi(&note[2]);
+	}
+	else
+	{
+		if (len == 1)
+			octave = 4;
+		else
+			octave = atoi(&note[1]);
+	}
+	frequency = get_note(note) * pow(2, octave);
+	//printf("octave is %d\n", octave);
+	return (frequency);
+}
+
+
+void	fill_node(t_node *list, char *note, float len)
+{
+	list->frequency = get_frequency(note);
+	// list->len = tempo / 60 * len;
+	list->len = len;
+	//printf("len is %f \n", tempo / 60 * len);
+	list->next = NULL;
+}
+
+void	feed_to_list(t_node **head, char *note, float len)
+{
+	t_node	*list;
+	t_node	*list_ptr;
+	t_node	*list_new;
+
+	// list = *head;
+	// while (list->next)
+	// 	list = list->next;
+	if (!*head)
+	{
+		list = (t_node *)malloc(sizeof(t_node *));
+		if (!list)
+			return ;
+		*head = list;
+		fill_node(list, note, len);
+	}
+	else
+	{
+		list_ptr = *head;
+		while (list_ptr->next)
+			list_ptr = list_ptr->next;
+		list_new = (t_node *)malloc(sizeof(t_node *));
+		if (!list_new)
+			return ;
+		fill_node(list_new, note, len);
+		list_ptr->next = list_new;
+	}
+}
+
+void	read_input(char *str, int tempo, t_node **head)
+// void	read_input(char *str, int tempo)
+{
+	// printf(" >>%s<< ", str);
 	int 	i = 0; 	// traverse str
-	int 	j = 0;	// compare to tracks
+	int 	j = 0;	// buffer note
 	char 	*tmp;
-	float 	len;
+	float 	len = 0.0;
 
 	tmp = (char*) malloc(4);
 	if (!tmp)
@@ -96,7 +203,7 @@ void	read_input(char *str, int tempo, t_node *data)
 		return ;
 	}
 
-	int track = atoi(str[i++]);	// add data to correct list
+	int track = atoi((const char*)&str[i]);	// add data to correct list
 	while (isdigit(str[i]) != 0 || str[i] == ' ' || str[i] == ':' || str[i] == '|')
 		i++;	// skip beginning
 
@@ -125,33 +232,69 @@ void	read_input(char *str, int tempo, t_node *data)
 			len = 1.0;
 		}
 		len = ((float)tempo / 60 * len);	// update beats to time
+		if (!head[track - 1])
+		{
+			head[track - 1] = (t_node *)malloc(sizeof(t_node *));
+			if (!head[track - 1])
+				return ;
+		}
+			// printf("%p\n", head_ptr);
+			// printf("%p\n", &head_ptr[0]);
+			// printf("%p\n", *head_ptr);
+			// printf("%p\n", head_ptr[1]);
 
-		data[track - 1]->len = len;
-		data[track - 1]->tone = tmp;
-
+		feed_to_list(&head[track - 1], tmp, len);
+		printf("(%s - %.3f)\t", tmp, len);
 
 		while (str[i] == ' ' || str[i] == '|')	// skip delimiters
 			i++;
 		// printf("%f << \n", len);
 	}
+	printf("\n");
 	free(tmp);
 }
 
-// typedef struct	s_node
-// {
-// 	float 			len;
-// 	char			*tone;
-// 	struct	s_node	*next;
-// }				t_node;
+void	print_list(t_node *head)
+{
+	t_node *list;
+
+	list = head;
+	while (list)
+	{
+		printf("'%f' - '%f'\t", list->frequency, list->len);
+		list = list->next;
+	}
+	printf("\n");
+}
+
+void	free_list(t_node **head)
+{
+	t_node *tmp;
+
+	if (!*head)
+	{
+		return ;
+	}
+	else
+	{
+		while ((*head)->next)
+		{
+			tmp = *head;
+			*head = (*head)->next;
+			free(tmp);
+		}
+		free(*head);
+	}
+}
 
 int main(int argc, char **argv)
 {
 	char	*ptr;
 	int		fd;
-	int		tracks;
-	int 	tempo;
+	int		tracks = 0;
+	int 	tempo = 0;
 	int		wave[50];	// store 'triange' etc as values
-	t_node 	*head;
+	t_node	**head_ptr = NULL;
 
 	if (argc != 2)
 		return (0);
@@ -159,7 +302,6 @@ int main(int argc, char **argv)
 	if (fd == -1)
 		return (0);
 
-	head = NULL;
 	while (get_next_line(fd, &ptr) == 1)
 	{
 		if (ptr[0] == '#' || strlen(ptr) == 0)
@@ -174,17 +316,43 @@ int main(int argc, char **argv)
 		else if (strncmp((const char*)ptr, "tracks", 6) == 0)
 		{
 			tracks = tracks_line(ptr, wave);
-			t_node data[tracks]; 	// each track has an node containing note + len
-			head = &data[0];
+
+			head_ptr = (t_node **) malloc(sizeof(t_node *) * tracks);
+			if (!head_ptr)
+			{
+				free(ptr);
+				return (0);
+			}
+			// printf("%p\n", head_ptr);
+			// printf("%p\n", &head_ptr[0]);
+			// printf("%p\n", *head_ptr);
+			// printf("%p\n", head_ptr[1]);
+			// printf("%p\n", &head_ptr);
+			// t_node ptr[tracks];
 		}
 		else
 		{
-			read_input(ptr, tempo, &data[0]);
+			// int channel = atoi((const char*)&ptr[0]);
+			// printf("	aaaaa	");
+			read_input(ptr, tempo, head_ptr);
+			// read_input(ptr, tempo);
+			// read_input(ptr, tempo);
+			// printf("	ccccc	");
 		}
 		// printf("%s\n", ptr);
 		free(ptr);
 	}
 	close(fd);
+	// int xx = 0;
+	// while (xx < tracks)
+	// {
+	// 	print_list(head_ptr[xx]);
+	// 	free_list(&head_ptr[xx]);
+	// 	xx++;
+	// }
+
+	free(head_ptr);
+
 				// test printing
 	// int x = 0;
 	// while (x < tracks)
